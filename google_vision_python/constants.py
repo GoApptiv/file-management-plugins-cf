57c0e77b7e0c7a49c486f6696cbf872db6da2c47
Shubham Ture <54004145+shubham-ture@users.noreply.github.com>
2022-04-22 14:18:27 +0530

    Cloud Function for Vision API (#1)

---
+++ google_vision_python/constants.py
@@ -0,0 +1,10 @@
+google_vision_api_key_path = 'your_vision_api_key.json'
+pubsub_key = 'your_pubsub_api_key.json'
+
+success_response = 'success'
+failure_response = 'failed'
+
+success_status_code = 200
+internal_server_error_code = 500
+
+pubsub_project_id = 'your_project_id'
