d8c470dd28087b93c0c1715a444964345c59cdff
sagar-vaghela97 <sagar.vaghela@goapptiv.com>
2022-07-05 13:27:40 +0530

    Add innokrit_inventions_invoice_ocr plugin

---
+++ innokrit_inventions_invoice_ocr_python/constants.py
@@ -0,0 +1,11 @@
+pubsub_key = 'your_pubsub_api_key.json'
+
+api_url = 'https://yourapiurl.com'
+
+success_response = 'success'
+failure_response = 'failed'
+
+success_status_code = 200
+internal_server_error_code = 500
+
+pubsub_project_id = 'your_project_id'
