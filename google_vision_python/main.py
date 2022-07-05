3e88c64d239840b13dfe7f7fb1cc25750c9bff84
greatecho <shubhamture@hotmail.com>
2022-07-05 14:28:33 +0530

    Added file upload verification and some minor changes

--- google_vision_python/main.py
+++ google_vision_python/main.py
@@ -1,4 +1,3 @@
-import urllib.request
 import os
 import json
 from google.cloud import storage, vision
@@ -7,16 +6,13 @@ from google.cloud import pubsub_v1
 import constants
 import base64
 import io
-import time
 import logging
 
 BASE_TEMP_DIR = "/tmp/"
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = constants.google_vision_api_key_path
 
-
 def store_output_to_bucket(event, context):
-
     try:
         pubsub_data_string = event['data']
         print(pubsub_data_string)
@@ -29,9 +25,11 @@ def store_output_to_bucket(event, context):
         file_name = None
         source_path = None
         destination_path = None
-        bucket_access_token = None
-        bucket_name = None
+        source_bucket_access_token = None
+        destination_bucket_access_token = None
         topic_name = None
+        destination_bucket_name = None
+        source_bucket_name = None
 
         if data.get('metadata'):
             uuid = data.get('metadata').get('uuid')
@@ -42,21 +40,25 @@ def store_output_to_bucket(event, context):
             if data.get('bucket').get('source'):
                 file_name = data.get('bucket').get('source').get('file')
                 source_path = data.get('bucket').get('source').get('path')
+                source_bucket_access_token = data.get('bucket').get('source').get('accessToken')
+                source_bucket_name = data.get('bucket').get('source').get('bucketName')
+
             if data.get('bucket').get('destination'):
                 destination_path = data.get('bucket').get('destination').get('path')
-                bucket_access_token = data.get('bucket').get('destination').get('accessToken')
-                bucket_name = data.get('bucket').get('destination').get('bucketName')
-    
+                destination_bucket_access_token = data.get('bucket').get('destination').get('accessToken')
+                destination_bucket_name = data.get('bucket').get('destination').get('bucketName')
+
         if data.get('response'):
             topic_name = data.get('response').get('topic')
 
-    
-        bucket_credentials = google.oauth2.credentials.Credentials(bucket_access_token)
+        source_bucket_credentiatials = google.oauth2.credentials.Credentials(source_bucket_access_token)
+        destination_bucket_credentials = google.oauth2.credentials.Credentials(destination_bucket_access_token)
 
         source_file_path = str(source_path) + '/' + str(file_name)
+        destination_file_path = str(destination_path + '/' + str(file_name))
 
         print("Downloading file from bucket")
-        file_details = download_from_bucket(bucket_name, file_name, project_id, bucket_credentials, source_file_path)
+        file_details = download_from_bucket(source_bucket_name, file_name, project_id, source_bucket_credentiatials, source_file_path)
 
         print("Checking if image is downloaded completely or not")
         if file_details[1] == (os.stat(file_details[0]).st_size):
@@ -71,29 +73,33 @@ def store_output_to_bucket(event, context):
             file_name_json_format = str(file_name_without_extension) + '.json'
 
             print("Uploading vision api response to bucket")
-            upload_to_bucket(vision_api_json_response, file_name, source_path, bucket_name, project_id, bucket_credentials)
-                    
-            print("Creating custom pub-sub object")
-            pubsub_object = create_pubsub_object(uuid, variant_id, destination_path, file_name_json_format, constants.success_response)
+            upload_to_bucket(vision_api_json_response, file_name, destination_file_path, destination_bucket_name, project_id,
+                             destination_bucket_credentials)
 
-            print(pubsub_object)
-            print("Publishing custom pub-sub object to topic")
-            publish_to_pubsub_topic(constants.pubsub_project_id, project_id, topic_name, pubsub_object)
+            print("Checking if the json file is uploaded or not")
+            file_check_response = check_if_file_exists(destination_bucket_credentials, destination_bucket_name, destination_file_path)
 
-            print("Deleting files from temporary directory")
-            delete_files_from_directory(BASE_TEMP_DIR)
+            if file_check_response == True:
+                print("Creating custom pub-sub object")
+                pubsub_object = create_pubsub_object(uuid, variant_id, destination_path, file_name_json_format, constants.success_response)
 
-            print('success response...')
-            return (constants.success_response, constants.success_status_code)      
+                print(pubsub_object)
+                print("Publishing custom pub-sub object to topic")
+                publish_to_pubsub_topic(constants.pubsub_project_id, project_id, topic_name, pubsub_object)
+
+                print("Deleting files from temporary directory")
+                delete_files_from_directory(BASE_TEMP_DIR)
+
+                print('success response...')
+                return (constants.success_response, constants.success_status_code)
 
     except Exception as e:
         logging.error(str(e))
-        pubsub_object = create_pubsub_object(uuid, variant_id, destination_path, file_name,
-                                             constants.failure_response)
-        publish_to_pubsub_topic(constants.pubsub_project_id, topic_name, pubsub_object)
+        pubsub_object = create_pubsub_object(uuid, variant_id, destination_path, file_name, constants.failure_response)
+        publish_to_pubsub_topic(constants.pubsub_project_id, project_id, topic_name, pubsub_object)
         print('failure response')
         return (constants.failure_response, constants.success_status_code)
-        
+
 
 def create_pubsub_object(uuid, variant_id, file_path, file_name, status):
     file_name_without_extension = os.path.splitext(file_name)[0]
@@ -153,7 +159,6 @@ def download_from_bucket(bucket_name, file_name, project_id, bucket_credentials,
 
 
 def run_vision_api_on_image(image_data):
-
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = constants.google_vision_api_key_path
     client = vision.ImageAnnotatorClient()
     image = vision.Image(content=image_data)
@@ -163,7 +168,6 @@ def run_vision_api_on_image(image_data):
 
 
 def upload_to_bucket(json_data, file_name, source_path, bucket_name, project_id, bucket_credentials):
-    
     file_name_without_extension = os.path.splitext(file_name)[0]
     file_name_json_format = file_name_without_extension + '.json'
     remote_json_file_path = source_path + '/'  + file_name_json_format
@@ -172,3 +176,19 @@ def upload_to_bucket(json_data, file_name, source_path, bucket_name, project_id,
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(remote_json_file_path)
     blob.upload_from_string(json_data, content_type='application/json')
+
+
+def check_if_file_exists(bucket_credentials, bucket_name, cloud_bucket_file_path):
+
+    storage_client = storage.Client(credentials=bucket_credentials)
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(cloud_bucket_file_path)
+    if blob.exists():
+        print(f'File {cloud_bucket_file_path} exists')
+        return blob.exists()
+    else:
+        print(f'File does not exist')
+        raise Exception("File does not exist")
+
+
+
